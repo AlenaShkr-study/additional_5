9a21feeb96dca83a0eedcd396aba2b11971ca723
AlenaShkr <elalshkr@gmail.com>
2018-09-29 21:23:21 +0300

    additional_5:Implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ 
+  let stack=[];
+  let lengthStr=str.length;
+  let lengthBrConfig=bracketsConfig.length;
+ 
+  for (let i=0; i<lengthStr; i++){
+    for (let j = 0; j < lengthBrConfig; j++){ 
+      if (bracketsConfig[j][0]!=bracketsConfig[j][1]){ 
+        if (str[i]===bracketsConfig[j][0]){
+        stack.push(str[i]);
+        } else  if ((str[i]===bracketsConfig[j][1]) && (stack.length!=0) && (bracketsConfig[j][0]===stack[stack.length-1])){
+        stack.pop();
+          }
+        }
+        else {
+       //if (bracketsConfig[j][0]==bracketsConfig[j][1]){  
+          if ((str[i]===bracketsConfig[j][0]) &&(bracketsConfig[j][0]!=stack[stack.length-1])){
+          stack.push(str[i]);
+        } else  if ((str[i]===bracketsConfig[j][1]) && (stack.length!=0) && (bracketsConfig[j][0]===stack[stack.length-1])){
+          stack.pop();
+      }
+    }
+  }
+}   
+     return (stack.length===0);
+} 
